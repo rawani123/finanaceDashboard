12ca2113a6b7f277d5a2d7ab4c7cc1d58c8425a0
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-04-05 10:41:35 +0530

    done

--- server/index.js
+++ server/index.js
@@ -42,6 +42,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
+    Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
